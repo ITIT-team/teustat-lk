dd73d7519e9c788bc9ab45c28edd09164edceb0c
FallenHopes <vlad.serebrow@yandex.ru>
2022-05-11 21:24:12 +0300

    feat(vlad): start backend

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 require('dotenv').config()
 
+const { myFetch } = require('./server/utils/myFetch')
+
 const port = process.env.PORT || 3001
 
 app.use(express.json({ extended: true }))
@@ -13,6 +15,16 @@ if (!process.env.DEV){
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
+
+app.post('*', async (req, res) => {
+    const resp = await myFetch({
+        path: '/userCompany',
+        body: { companyId: "280b908d-726b-11eb-9c66-ac1f6b817986" },
+        headers: req.headers
+    })
+    console.log(resp)
+    return res.status(200).json(resp)
+})
 
 app.listen(port, err => {
     if (err){
