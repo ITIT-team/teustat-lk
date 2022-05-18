d0ba9b28e2f3c23400508df6bc6f2acc25c0a2de
MAN5ON <dark34rus@mail.ru>
2022-05-18 14:20:33 +0300

    feat(authpage):create toggle-switch sample

---
+++ client/src/samples/ToggleSwitch/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import st from './../../styles/samples/toggle_switch.module.css'
+
+export const ToggleSwitch = ({
+    name,
+    value,
+    onChange
+}) => {
+    return (
+        <div className={st.toggle_switch}>
+            <label className={st.switch}>
+                <input
+                    name={name}
+                    type="checkbox"
+                    value={value}
+                    onChange={onChange}
+                />
+                <span className={st.slider} />
+            </label>
+        </div>
+    )
+}
