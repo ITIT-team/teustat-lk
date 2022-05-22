ba301414216cfa54ec0eaffe5f6b7dc6e41d57c3
FallenHopes <vlad.serebrow@yandex.ru>
2022-05-22 23:44:19 +0300

    feat(global): logout

--- client/src/pages/UserSpace/index.js
+++ client/src/pages/UserSpace/index.js
@@ -1,33 +1,61 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
-import navlink_st from '../../styles/UserSpace/navlink.module.css'
-import header_st from '../../styles/UserSpace/header.module.css'
-import '../../styles/UserSpace/main_styles.css'
+import { useHttp } from 'hooks'
+import { useMyContext } from 'Context'
+import { Loader } from 'samples/Loader'
+import navlink_st from 'styles/UserSpace/navlink.module.css'
+import header_st from 'styles/UserSpace/header.module.css'
+import 'styles/UserSpace/main_styles.css'
 
-import mainLogo from '../../assets/main/logo.svg'
+import mainLogo from 'assets/main/logo.svg'
 
 const setActiveLink = ({ isActive }) => isActive ? navlink_st.active_navlink : navlink_st.passive_navlink
 
 export const UserSpace = () => {
+    const { request, loading } = useHttp()
+    const { userData, setUserData } = useMyContext()
+
+    const logoutHandler = async () => {
+        try {
+            await request('/auth/logout')
+            setUserData(null)
+        } catch (e) {
+            console.warn(e)
+        }
+    }
+
     return (
-        <section>
-            <header>
-                <div className={header_st.logo_container}>
-                    <img src={mainLogo} alt='Teustat' />
-                    <h3>TEUSTAT</h3>
-                </div>
-                <ul className={header_st.main_menu}>
-                    <li>
-                        <NavLink to='clients' className={setActiveLink}>Клиенты</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='test-access' className={setActiveLink}>Тестовый доступ</NavLink>
-                    </li>
-                </ul>
-            </header>
-            <main>
-                <Outlet />
-            </main>
-        </section>
+        <>
+            {
+                loading ?
+                <Loader />
+                :
+                <section>
+                    <header>
+                        <div className={header_st.logo_container}>
+                            <img src={mainLogo} alt='Teustat' />
+                            <h3>{userData.name}</h3>
+                        </div>
+                        <ul className={header_st.main_menu}>
+                            <li>
+                                <NavLink to='clients' className={setActiveLink}>Клиенты</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='test-access' className={setActiveLink}>Тестовый доступ</NavLink>
+                            </li>
+                            <li>
+                                <span
+                                    className={navlink_st.passive_navlink}
+                                    onClick={logoutHandler}
+                                >Выйти</span>
+                            </li>
+                        </ul>
+                    </header>
+                    <main>
+                        <Outlet />
+                    </main>
+                </section>
+            }
+        </>
     )
 }
