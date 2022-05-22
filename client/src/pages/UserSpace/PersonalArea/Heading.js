fe51c487ebb9872d2e7f65903a0fcbab1a54b857
FallenHopes <vlad.serebrow@yandex.ru>
2022-05-22 13:48:39 +0300

    feat(global): short imports and finish heading

--- client/src/pages/UserSpace/PersonalArea/Heading.js
+++ client/src/pages/UserSpace/PersonalArea/Heading.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { SimpleTextInput } from '../../../samples/SimpleTextInput'
-import st from '../../../styles/UserSpace/personal_area.module.css'
+import { SimpleTextInput } from 'samples/SimpleTextInput'
+import { SimpleSelect } from 'samples/SimpleSelect'
+import st from 'styles/UserSpace/personal_area.module.css'
 
 export const Heading = ({ filters, filtersHandler }) => {
     return (
@@ -12,17 +13,37 @@ export const Heading = ({ filters, filtersHandler }) => {
                     <div className={st.button_text}>Добавить клиента</div>
                 </div>
             </div>
+            {/* TODO hidden panel for marked lines */}
+            {/* <div className={st.hidden_panel}>
+
+            </div> */}
             <div className={st.heading_filters}>
-                <SimpleTextInput
-                    name='search'
-                    placeholder='Поиск'
-                    symbol='&#128269;'
-                    value={filters.search}
-                    onChange={filtersHandler}
-                    bold
-                />
+                <div className={st.search}>
+                    <SimpleTextInput
+                        name='search'
+                        placeholder='Поиск'
+                        symbol='&#128269;'
+                        value={filters.search}
+                        onChange={filtersHandler}
+                        bold
+                    />
+                </div>
+                <div className={st.sort_by}>
+                    <SimpleSelect
+                        options={['Дате', 'Имени', 'Пользователям']}
+                        value={filters.sort}
+                        setNewValue={val => {
+                            const fakeEvent = {
+                                target: {
+                                    name: 'sort',
+                                    value: val
+                                }
+                            }
+                            filtersHandler(fakeEvent)
+                        }}
+                    />
+                </div>
             </div>
-            
         </div>
     )
 }
---
+++ client/src/samples/SimpleSelect/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from 'react'
+import st from 'styles/samples/simple_select.module.css'
+
+export const SimpleSelect = ({
+    text='Сортировать по:',
+    options=[""],
+    value,
+    setNewValue
+}) => {
+    const [opened, setOpened] = useState(false)
+    const mainRef = useRef()
+
+    useEffect(() => {
+        const docClickListener = () => setOpened(false)
+        document.addEventListener('click', docClickListener)
+        mainRef.current.addEventListener('mouseenter', () => 
+            document.removeEventListener('click', docClickListener)
+        )
+        mainRef.current.addEventListener('mouseleave', () =>
+            document.addEventListener('click', docClickListener)
+        )
+        return () => {
+            document.removeEventListener('click', docClickListener)
+        }
+    }, [])
+
+    return (
+        <div className={st.simple_select_container} ref={mainRef}>
+            <span className={st.simple_select_text}>{text}</span>
+            <div className={st.simple_select}>
+                <div
+                    className={st.simple_select_result}
+                    onClick={() => setOpened(prev => !prev)}
+                >
+                    {value}
+                    &nbsp;
+                    <div
+                        style={{
+                            width: 10,
+                            height: 10,
+                            fontSize: 10,
+                            marginBottom: 2,
+                            transform: opened ? 'rotateZ(180deg)' : '',
+                            transition: 'transform .2s ease-in-out'
+                            
+                        }}
+                    >&#9660;</div>
+                </div>
+                <div
+                    className={st.simple_select_options + (opened ? ` ${st.options_active}` : '')}
+                >
+                    {
+                        options.map(
+                            op => <div
+                                key={op}
+                                className={st.simple_option}
+                                onClick={() => {
+                                    setNewValue(op)
+                                    setOpened(false)
+                                }}
+                            >{op}</div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
