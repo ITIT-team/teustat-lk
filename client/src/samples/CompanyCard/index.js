d35adfa748959a361c2393ef7db1d981689471f2
MAN5ON <dark34rus@mail.ru>
2022-05-22 23:44:28 +0300

    feat(renat):add company card

---
+++ client/src/samples/CompanyCard/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { RadioButton } from "samples/RadioButton";
+import { ToggleSwitch } from "samples/ToggleSwitch";
+import { TrashIcon } from "samples/TrashIcon";
+import st from 'styles/samples/company_card.module.css'
+import { convertDate } from 'utils/convertDate'
+
+
+export const CompanyCard = ({ company }) => {
+    return (
+        <div className={st.company_card}>
+            <div className={st.name_section}>
+                <RadioButton checked={false} setChecked={() => { }} />
+                <div className={st.company_name}>{company.name}</div>
+            </div>
+
+            <div className={st.users_count}>
+                <div className={st.users_count_num}>{company.employeeCount}</div> пользователей</div>
+            <div className={st.activated_change_date}>
+                {
+                    company.activated ?
+                        'Подключен '
+                        :
+                        'Отключен '
+                }
+                {
+                    convertDate(company.activatedChangeDate)
+                }
+            </div>
+
+            <div className={st.icons_section}>
+                <ToggleSwitch
+                    name='company_activate'
+                    value={company.activated}
+                />
+                <TrashIcon />
+                <div className={st.arrow}></div>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/samples/RadioButton/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import st from 'styles/samples/radio_btn.module.css'
+
+export const RadioButton = ({ checked, setChecked }) =>
+    <div
+        className={st.radio}
+        style={{
+            border: checked ? '6px solid var(--hardBlue)' : '1px solid rgba(128, 137, 154, 0.6)'
+        }}
+        onClick={setChecked.bind(this, !checked)}
+    ></div>
