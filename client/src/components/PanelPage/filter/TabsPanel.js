15dd860cb44d73c061ad87c10c12fa474607a874
FallenHopes <vlad.serebrow@yandex.ru>
2022-08-17 18:58:30 +0300

    feat(tabs): new tabs style (and constants)

--- client/src/components/PanelPage/filter/TabsPanel.js
+++ client/src/components/PanelPage/filter/TabsPanel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TAB_ID } from 'constants/PanelConstants'
 import c from 'styles/PanelPage/filter/tabspanel.module.css'
 import fraxtIcon from 'assets/panel/tabspanel/fraxt_icon.svg'
 import activeFraxtIcon from 'assets/panel/tabspanel/active_fraxt_icon.svg'
@@ -16,37 +17,74 @@ import activeGroupageIcon from 'assets/panel/tabspanel/active_groupage_icon.svg'
 // import activeGraphicsIcon from 'assets/panel/tabspanel/active_graphics_icon.svg'
 import mapsIcon from 'assets/panel/tabspanel/maps_icon.svg'
 import activeMapsIcon from 'assets/panel/tabspanel/active_maps_icon.svg'
+import { ReactComponent as BlueContainerIcon } from 'assets/panel/tabspanel/groupageswitcher/container_blue_icon.svg'
+import { ReactComponent as LightBlueContainerIcon } from 'assets/panel/tabspanel/groupageswitcher/container_light_blue_icon.svg'
+import { ReactComponent as BlueGroupageIcon } from 'assets/panel/tabspanel/groupageswitcher/groupage_blue_icon.svg'
+import { ReactComponent as LightBlueGroupageIcon } from 'assets/panel/tabspanel/groupageswitcher/groupage_light_blue_icon.svg'
 
 const dataLayer = {
-    1: { blue: fraxtIcon, white: activeFraxtIcon, tabName: <>ФРАХТ</> },
-    2: { blue: jdIcon, white: activeJdIcon, tabName: <>ЖД</> },
-    3: { blue: autoIcon, white: activeAutoIcon, tabName: <>АВТОВЫВОЗ</> },
-    4: { blue: givenIcon, white: activeGivenIcon, tabName: <>ВЫДАЧА /<br />АРЕНДА КТК</> },
-    5: { blue: crossIcon, white: activeCrossIcon, tabName: <>СКВОЗНЫЕ<br />СЕРВИСЫ</> },
-    6: { blue: groupageIcon, white: activeGroupageIcon, tabName: <>СБОРНЫЕ ГРУЗЫ</> },
-    7: { blue: mapsIcon, white: activeMapsIcon, tabName: <>КАРТА<br />ТЕРМИНАЛОВ</> },
+    [TAB_ID.FRAXT]: { blue: fraxtIcon, white: activeFraxtIcon, tabName: <>ФРАХТ</> },
+    [TAB_ID.JD]: { blue: jdIcon, white: activeJdIcon, tabName: <>ЖД</> },
+    [TAB_ID.AUTO]: { blue: autoIcon, white: activeAutoIcon, tabName: <>АВТОВЫВОЗ</> },
+    [TAB_ID.GIVEN]: { blue: givenIcon, white: activeGivenIcon, tabName: <>ВЫДАЧА /<br />АРЕНДА КТК</> },
+    [TAB_ID.CROSS]: { blue: crossIcon, white: activeCrossIcon, tabName: <>СКВОЗНЫЕ<br />СЕРВИСЫ</> },
+    [TAB_ID.GROUPAGE]: { blue: groupageIcon, white: activeGroupageIcon, tabName: <>СБОРНЫЕ ГРУЗЫ</> },
+    [TAB_ID.MAP]: { blue: mapsIcon, white: activeMapsIcon, tabName: <>КАРТА<br />ТЕРМИНАЛОВ</> },
 }
 
-export const TabsPanel = ({ tabs, activetab, setActivetab }) => 
-    <div className={c.tabs}>
-        {
-            tabs.map(t => (
-                <div 
-                    key={t.id} 
+export const TabsPanel = ({ tabs, activetab, setActivetab }) => {
+    return (
+        <div className={c.tabs}>
+            <div
+                className={c.tab}
+                style={{
+                    backgroundColor: 'var(--lightBlue)',
+                    border: 'none',
+                    justifySelf: 'flex-start',
+                }}
+            >
+                <div
+                    className={c.tab_switch}
                     style={{
-                        backgroundColor: t.id === activetab ? 'var(--hardBlue)' : 'transparent'
-                    }} 
-                    className={c.tab}
-                    onClick={() => setActivetab(t.id)}
+                        backgroundColor: activetab !== TAB_ID.GROUPAGE ? 'white' : 'transparent'
+                    }}
+                    onClick={setActivetab.bind(this, TAB_ID.CROSS)}
                 >
-                    <div className={c.tab_logo}>
-                        <img src={t.id === activetab ? dataLayer[t.id].white : dataLayer[t.id].blue} alt={dataLayer[t.id].tabName} />
-                    </div>
-                    <div
-                        style={t.id === activetab ? { color: 'white' } : {}}
-                        className={c.tab_name}
-                    >{dataLayer[t.id].tabName}</div>
+                    { activetab !== TAB_ID.GROUPAGE ? <BlueContainerIcon /> : <LightBlueContainerIcon /> }
                 </div>
-            ))
-        }
-    </div>
+                <div
+                    className={c.tab_switch}
+                    style={{
+                        backgroundColor: activetab === TAB_ID.GROUPAGE ? 'white' : 'transparent'
+                    }}
+                    onClick={setActivetab.bind(this, TAB_ID.GROUPAGE)}
+                >
+                    { activetab === TAB_ID.GROUPAGE ? <BlueGroupageIcon /> : <LightBlueGroupageIcon /> }
+                </div>
+            </div>
+            {
+                activetab !== TAB_ID.GROUPAGE &&
+                tabs.filter(t => t.id !== TAB_ID.GROUPAGE).map(t => {
+                    return (
+                        <div 
+                            key={t.id} 
+                            style={{
+                                backgroundColor: t.id === activetab ? 'var(--hardBlue)' : 'transparent'
+                            }} 
+                            className={c.tab}
+                            onClick={setActivetab.bind(this, t.id)}
+                        >
+                            <div className={c.tab_logo}>
+                                <img src={t.id === activetab ? dataLayer[t.id].white : dataLayer[t.id].blue} alt={dataLayer[t.id].tabName} />
+                            </div>
+                            <div
+                                style={t.id === activetab ? { color: 'white' } : {}}
+                                className={c.tab_name}
+                            >{dataLayer[t.id].tabName}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- client/src/components/PanelPage/filter/tabs/CrossTab.js
+++ client/src/components/PanelPage/filter/tabs/CrossTab.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { TAB_ID } from 'constants/PanelConstants'
 import { Select } from 'components/Global/Select'
 import { ThumblersRow } from 'components/PanelPage/ThumblersRow'
 import { Thumbler } from 'components/PanelPage/Thumbler'
@@ -36,7 +37,7 @@ export const CrossTab = ({ filters, setFilter, clearFilters }) => {
         }
     }, [ setPulse ])
 
-    const data = records.find(r => r.id === 5).recs
+    const data = records.find(r => r.id === TAB_ID.CROSS).recs
 
     const depCities = departureCities(data, filters)
     const desCities = destinationCities(data, filters)
