619a5ed03f9959f2105744d9ecac22ff443ec219
FallenHopes <vlad.serebrow@yandex.ru>
2022-08-25 18:12:03 +0300

    fix(pdfreader): new pdf reader logic

--- client/src/components/UserSpace/PdfReader/index.js
+++ client/src/components/UserSpace/PdfReader/index.js
@@ -1,25 +1,29 @@
 import React from 'react'
+import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { usePanelContext } from 'Context'
 import c from 'styles/UserSpace/pdf_reader.module.css'
 
 export const PdfReader = ({ name, data }) => {
     const { setPdf } = usePanelContext()
+
+    const bytes = atob(data)
+    let length = bytes.length
+    let out = new Uint8Array(length)
+
+    while (length--) {
+        out[length] = bytes.charCodeAt(length);
+    }
+
+    const url = URL.createObjectURL(new Blob([out], { type: 'application/pdf' }));
+
     return (
         <div className={c.pdf_reader_area}>
             <div className={c.pdf_reader_container}>
                 <div className={c.pdf_reader_close} onClick={() => setPdf(null)}>&times;</div>
                 <div className={c.pdf_reader_name}>{name}</div>
-                <object
-                    data={`data:application/pdf;base64,${data}`}
-                    type="application/pdf"
-                    className={c.pdf_reader_obj}
-                    name={name}
-                >
-                    <embed
-                        src={`data:application/pdf;base64,${data}`}
-                        type="application/pdf"
-                    ></embed>
-                </object>
+                <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js'>
+                    <Viewer fileUrl={url} />
+                </Worker>
             </div>
         </div>
     )
