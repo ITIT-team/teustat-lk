414957574282633e2878fbeb77a14ccd9aa435de
Vladislav Serebrov <48473470+FallenHopes@users.noreply.github.com>
2022-08-26 17:28:24 +0300

    fix(pdfreader): new pdf reader logic (#53)

--- client/src/components/UserSpace/PdfReader/index.js
+++ client/src/components/UserSpace/PdfReader/index.js
@@ -1,26 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Viewer, Worker } from '@react-pdf-viewer/core'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import { getFilePlugin } from '@react-pdf-viewer/get-file'
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar'
 import { usePanelContext } from 'Context'
+import { BlurPage } from 'components/Global/BlurPage'
 import c from 'styles/UserSpace/pdf_reader.module.css'
+
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
 export const PdfReader = ({ name, data }) => {
     const { setPdf } = usePanelContext()
+    const [url] = useState(() => {
+        const bytes = atob(data)
+        let length = bytes.length
+        let out = new Uint8Array(length)
+        while (length--) {
+            out[length] = bytes.charCodeAt(length);
+        }
+        return URL.createObjectURL(new Blob([out], { type: 'application/pdf' }))
+    })
+
+    const instanceOfFilenamePlugin = getFilePlugin({ fileNameGenerator: () => `[teustat] ${name}.pdf` })
+    const instanceOfToolbarPlugin = toolbarPlugin()
+    const { renderDefaultToolbar, Toolbar } = instanceOfToolbarPlugin
+    const { DownloadButton } = instanceOfFilenamePlugin
+
+    const renderToolbar = () => (
+        <Toolbar>
+            {
+                renderDefaultToolbar(
+                    slot => ({
+                        ...slot,
+                        Download: () => <DownloadButton />
+                    })
+                )
+            }
+        </Toolbar>
+    )
+
+    const instanceOfLayoutPlugin = defaultLayoutPlugin({
+        sidebarTabs: defaultTabs => [ defaultTabs[0] ],
+        renderToolbar
+    })
+
     return (
-        <div className={c.pdf_reader_area}>
-            <div className={c.pdf_reader_container}>
+        <BlurPage>
+            <div className={c.pdf_reader_container} id='pdf_reader'>
                 <div className={c.pdf_reader_close} onClick={() => setPdf(null)}>&times;</div>
                 <div className={c.pdf_reader_name}>{name}</div>
-                <object
-                    data={`data:application/pdf;base64,${data}`}
-                    type="application/pdf"
-                    className={c.pdf_reader_obj}
-                    name={name}
-                >
-                    <embed
-                        src={`data:application/pdf;base64,${data}`}
-                        type="application/pdf"
-                    ></embed>
-                </object>
+                <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js'>
+                    <Viewer
+                        fileUrl={url}
+                        plugins={[ instanceOfToolbarPlugin, instanceOfFilenamePlugin, instanceOfLayoutPlugin ]}
+                    />
+                </Worker>
             </div>
-        </div>
+        </BlurPage>
     )
 }
