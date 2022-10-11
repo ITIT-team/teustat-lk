dd835e42a764ad2b74804d6a75a767f1fcbea386
FallenHopes <vlad.serebrow@yandex.ru>
2022-10-11 14:48:51 +0300

    feat(panel): new flags and relocate imports

--- client/src/components/Global/Select/index.js
+++ client/src/components/Global/Select/index.js
@@ -1,72 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useGlobalContext } from 'Context'
+import { CountryIcons } from 'constants/CountryIcons'
 import { alphaviteSorter } from 'utils'
 import c from 'styles/components/select.module.css';
 
-import vietnamIcon from 'assets/panel/countries/vietnam_icon.png'
-import indiaIcon from 'assets/panel/countries/india_icon.png'
-import kazakhstanIcon from 'assets/panel/countries/kazakhstan_icon.png'
-import chinaIcon from 'assets/panel/countries/china_icon.png'
-import koreaIcon from 'assets/panel/countries/korea_icon.png'
-import laosIcon from 'assets/panel/countries/laos_icon.png'
-import russiaIcon from 'assets/panel/countries/russia_icon.png'
-import singaporeIcon from 'assets/panel/countries/singapore_icon.png'
-import thailandIcon from 'assets/panel/countries/thailand_icon.png'
-import taiwanIcon from 'assets/panel/countries/taiwan_icon.png'
-import turkeyIcon from 'assets/panel/countries/turkey_icon.png'
-import japanIcon from 'assets/panel/countries/japan_icon.png'
-import malaysiaIcon from 'assets/panel/countries/malaysia_icon.png'
-import belarusIcon from 'assets/panel/countries/belarus_icon.png'
-import cambodiaIcon from 'assets/panel/countries/cambodia_icon.png'
-import bangladeshIcon from 'assets/panel/countries/bangladesh_icon.png'
-import philippinesIcon from 'assets/panel/countries/philippines_icon.png'
-import indonesiaIcon from 'assets/panel/countries/indonesia_icon.png'
-import usaIcon from 'assets/panel/countries/usa_icon.png'
-import germanyIcon from 'assets/panel/countries/germany_icon.png'
-import pakistanIcon from 'assets/panel/countries/pakistan_icon.png'
-import arabianIcon from 'assets/panel/countries/arabian_icon.png'
-import uzbekistanIcon from 'assets/panel/countries/uzbekistan_icon.png'
-import brazilIcon from 'assets/panel/countries/brazil_icon.png'
-import egyptIcon from 'assets/panel/countries/egypt_icon.png'
-import izrailIcon from 'assets/panel/countries/izrail_icon.png'
-import netherlandsIcon from 'assets/panel/countries/netherlands_icon.png'
-import australiaIcon from 'assets/panel/countries/australia_icon.png'
-import saudiIcon from 'assets/panel/countries/saudi_icon.png'
-
 import { PanelLocale, CountriesLocale } from 'locales'
-
-const countryIcons = {
-    "ВЬЕТНАМ": vietnamIcon,
-    "ИНДИЯ": indiaIcon,
-    "КАЗАХСТАН": kazakhstanIcon,
-    "КИТАЙ": chinaIcon,
-    "КОРЕЯ, РЕСПУБЛИКА": koreaIcon,
-    "ЛАОССКАЯ НАРОДНО-ДЕМОКРАТИЧЕСКАЯ РЕСПУБЛИКА": laosIcon,
-    "РОССИЯ": russiaIcon,
-    "СИНГАПУР": singaporeIcon,
-    "ТАИЛАНД": thailandIcon,
-    "ТАЙВАНЬ (КИТАЙ)": taiwanIcon,
-    "Тайвань": taiwanIcon,
-    "ТУРЦИЯ": turkeyIcon,
-    "ЯПОНИЯ": japanIcon,
-    "МАЛАЙЗИЯ": malaysiaIcon,
-    "БЕЛАРУСЬ": belarusIcon,
-    "КАМБОДЖА": cambodiaIcon,
-    "БАНГЛАДЕШ": bangladeshIcon,
-    "ФИЛИППИНЫ": philippinesIcon,
-    "ИНДОНЕЗИЯ": indonesiaIcon,
-    "СОЕДИНЕННЫЕ ШТАТЫ": usaIcon,
-    "ГЕРМАНИЯ": germanyIcon,
-    "ПАКИСТАН": pakistanIcon,
-    "ОБЪЕДИНЕННЫЕ АРАБСКИЕ ЭМИРАТЫ": arabianIcon,
-    "УЗБЕКИСТАН": uzbekistanIcon,
-    "БРАЗИЛИЯ": brazilIcon,
-    "ЕГИПЕТ": egyptIcon,
-    "ИЗРАИЛЬ": izrailIcon,
-    "НИДЕРЛАНДЫ": netherlandsIcon,
-    "АВСТРАЛИЯ": australiaIcon,
-    "САУДОВСКАЯ АРАВИЯ": saudiIcon,
-}
 
 export const Select = ({
     items,
@@ -208,7 +146,7 @@ export const Select = ({
                                     onClick={() => changeValue(item.city)}
                                 >
                                     <div className={c.city_container}>
-                                        <img className={c.flag_icon} src={countryIcons[item.country]} alt={item.country}/>
+                                        <img className={c.flag_icon} src={CountryIcons[item.country]} alt={item.country}/>
                                         <div className={c.city_name}>{item.city}</div>
                                     </div>
                                     <div className={c.country_name}>{CountriesLocale[item.country]?.[locale] || item.country}</div>
