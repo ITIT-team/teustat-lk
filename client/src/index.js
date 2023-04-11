4c15e580fbc7054ecaac9e3ddfa9392eb72c679e
Vladislav Serebrov <48473470+FallenHopes@users.noreply.github.com>
2023-04-11 10:39:36 +0300

    feat: Add Sentry logging to project (#109)
    feat: Add Sentry logging to project (#109)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import { App } from './App'
+
+Sentry.init({
+  dsn: "https://85139dfe5cbc417dbebf24638029ec61@o4504994273296384.ingest.sentry.io/4504994278998016",
+  integrations: [new Sentry.BrowserTracing()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+})
 
 createRoot(document.getElementById('root')).render( <App /> )
